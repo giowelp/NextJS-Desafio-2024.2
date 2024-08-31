13cb7347f47e3a8e14dcd166787c79564e15e069
welp <giovanijws@gmail.com>
2024-08-31 19:49:35 -0300

    feat: importing local fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,49 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+
+
+const helvetica = localFont({
+  src: [
+    {
+      path: "../public/fonts/HELVETICA.ttf",
+    },
+  ],
+  variable: "--font-helvetica",
+});
+
+const helveticaRounded = localFont({
+  src: [
+    {
+      path: "../public/fonts/HELVETICA-ROUNDED.ttf",
+    },
+  ],
+  variable: "--font-helveticaRounded",
+});
+
+const helveticaLight = localFont({
+  src: [
+    {
+      path: "../public/fonts/HELVETICA-LIGHT.otf",
+    },
+  ],
+  variable: "--font-helveticaLight",
+});
+
+const helveticaBold = localFont({
+  src: [
+    {
+      path: "../public/fonts/HELVETICA-BOLD.ttf",
+    },
+  ],
+  variable: "--font-helveticaBold",
+});
 
 export const metadata: Metadata = {
-  title: "Desafio Next.JS 2024.2",
-  description: "Desafio Next.JS, Typescript, Tailinwd, Prisma, semestre 2024.2 para todos os membros da Code JR",
+  title: "VANGUARD",
+  description: "Desafio Next.JS, Typescript, Tailwind, Prisma, semestre 2024.2 para todos os membros da Code JR",
 };
 
 export default function RootLayout({
@@ -15,8 +52,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html
+      lang="en"
+      className={`${helvetica.variable} ${helveticaRounded.variable} ${helveticaLight.variable} ${helveticaBold.variable}`}
+    >
+      <body>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        helvetica: ["var(--font-helvetica)"],
+        helveticaRounded: ["var(--font-helveticaRounded)"],
+        helveticaLight: ["var(--font-helveticaLight)"],  
+        helveticaBold: ["var(--font-helveticaBold)"], 
+      },
+      screens: {
+        'md': '768px',
+        'lg': '1280px',
+        'xl': '1920x',
+        '2xl': '2560px',
+        '3xl': '3440px',
+      }
     },
   },
   plugins: [],
